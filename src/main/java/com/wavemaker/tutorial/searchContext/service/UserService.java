9192e5d3419ea41df898c29e6d0362644d6facc7
srujans16 <srujans1695@gmail.com>
2016-07-26 15:14:43 +0530

    method inserUser renamed to insertUser

--- src/main/java/com/wavemaker/tutorial/searchContext/service/UserService.java
+++ src/main/java/com/wavemaker/tutorial/searchContext/service/UserService.java
@@ -3,6 +3,12 @@ package com.wavemaker.tutorial.searchContext.service;
 import com.wavemaker.tutorial.searchContext.domain.User;
 import com.wavemaker.tutorial.searchContext.util.DbConnectionUtil;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,10 +27,9 @@ public class UserService {
             ResultSet resultSet = preparedStatement.executeQuery();
             if (!resultSet.next()) {
                 return null;
-            } else {
-                User user = new User(resultSet.getString("emailId"), resultSet.getString("password"), resultSet.getString("username"));
-                return user;
             }
+            User user = new User(resultSet.getString("emailId"), resultSet.getString("password"), resultSet.getString("username"));
+            return user;
 
         } catch (SQLException e) {
             throw new RuntimeException("Database access error", e);
@@ -39,7 +44,7 @@ public class UserService {
         }
     }
 
-    public static int inserUser(User user) {
+    public static int insertUser(User user) {
         try {
             String query = "insert into users(username,password,emailId) values(?,?,?)";
             PreparedStatement preparedStatement = getPreparedStatement(query);
@@ -53,8 +58,10 @@ public class UserService {
 
     }
 
+
     private static PreparedStatement getPreparedStatement(String query) throws SQLException {
-        PreparedStatement preparedStatement = DbConnectionUtil.getConnection().prepareStatement(query);
+        Connection connection=DbConnectionUtil.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
         return preparedStatement;
     }
 
